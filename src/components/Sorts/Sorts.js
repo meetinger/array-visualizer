e99088cbe2f20ef940d3ded612365b85b696baa4
meetinger <meetingerdev@gmail.com>
2021-07-08 23:38:56 +0300

    Add InsertionSort

--- src/components/Sorts/Sorts.js
+++ src/components/Sorts/Sorts.js
@@ -156,4 +156,17 @@ export class Sorts {
         }
     }
 
+    InsertionSort(){
+        let length = this.arrLength;
+        for (let i = 1; i < length; i++) {
+            let key = this.read(i);
+            let j = i - 1;
+            while (j >= 0 && this.read(j) > key) {
+                this.write(j+1, this.read(j))
+                j = j - 1;
+            }
+            this.write(j+1, key)
+        }
+    }
+
 }
