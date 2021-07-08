1fabbaa341040f144e297e045a3a6abcc6d6a477
meetinger <meetingerdev@gmail.com>
2021-07-08 22:47:06 +0300

    Stats Sort Bold

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -4,7 +4,7 @@ import styles from "./Stats.module.scss";
 export function Stats(props){
     return(
         <div className={styles.statsContainer}>
-            <div>Sort: {props.sortName}</div>
+            <div><b>Sort: {props.sortName}</b></div>
             <div>Length: {props.arrLength}</div>
             {/*<div>Comparisons: {props.comparisons}</div>*/}
             <div>Writes: {props.writes}</div>
