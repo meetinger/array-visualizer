ca9b49d81778091c290833ced89ee719845e75ac
meetinger <meetingerdev@gmail.com>
2021-07-13 07:03:10 +0300

    Add LRQuickSort

---
+++ src/components/Sorts/LRQuickSort.js
@@ -0,0 +1,42 @@
+import {Sort} from "./Sort";
+
+export class LRQuickSort extends Sort{
+    constructor(arrayVisualizer) {
+        super(arrayVisualizer);
+        this.sortName="LRQuickSort"
+    }
+
+    quickSort(p, r){
+        let pivot = Math.trunc(p + (r - p + 1) / 2);
+        let x = this.read(pivot);
+
+        let i = p;
+        let j = r;
+
+        while (i <= j) {
+            while (this.read(i) < x){
+                i++;
+            }
+            while (this.read(j) > x){
+                j--;
+            }
+
+            if (i <= j) {
+                this.swap(i, j);
+                i++;
+                j--;
+            }
+        }
+
+        if(p < j) {
+            this.quickSort(p, j);
+        }
+        if(i < r) {
+            this.quickSort(i, r);
+        }
+    }
+
+    runSort(low, high) {
+        this.quickSort(low, high)
+    }
+}
