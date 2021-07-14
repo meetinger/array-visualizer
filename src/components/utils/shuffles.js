598f5fd072e021abcd97f721644325987ef794c4
meetinger <meetingerdev@gmail.com>
2021-07-14 03:36:06 +0300

    Add the stable shuffling

--- src/components/utils/shuffles.js
+++ src/components/utils/shuffles.js
@@ -16,25 +16,10 @@ export let shuffles = {
         return instructions
     },
 
-    stableShuffle: function (len) {
-        let instructions = []
-        for (let i = 0; i < len; ++i) {
-            let randomIndex = randomInt(i+1, len)
-            instructions.push(
-                {
-                    cmd: "swap",
-                    a: i,
-                    b:randomIndex
-                }
-            )
-        }
-        return instructions
-    },
-
     almostSorted: function (len) {
         let instructions = []
-        const amount = 0.1
-        for (let i = 0; i < len * amount; ++i) {
+        const AMOUNT = 0.1
+        for (let i = 0; i < len * AMOUNT; ++i) {
             let randomIndexA = randomInt(i, len)
             let randomIndexB = randomInt(i, len)
             instructions.push(
@@ -60,6 +45,23 @@ export let shuffles = {
             )
         }
         return instructions
+    },
+    stableShuffle: function (len) {
+        const GAP_FACTOR = 10
+        let gap = len/GAP_FACTOR
+        let instructions = []
+        for (let i = 0; i < GAP_FACTOR-1; ++i) {
+            let factor = randomInt(i+1, GAP_FACTOR)
+            for(let j = 0; j < gap; ++j){
+                instructions.push(
+                    {
+                        cmd: "swap",
+                        a: j+i*gap,
+                        b: j+factor*gap
+                    }
+                )
+            }
+        }
+        return instructions
     }
-
 }
