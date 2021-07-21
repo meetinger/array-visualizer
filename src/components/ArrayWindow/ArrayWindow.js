1c9c069ec774f7554e0a983c5f07b0697a9d7ade
meetinger <meetingerdev@gmail.com>
2021-07-21 20:31:04 +0300

    Add dots style to canvas(replace html to canvas)

--- src/components/ArrayWindow/ArrayWindow.js
+++ src/components/ArrayWindow/ArrayWindow.js
@@ -20,7 +20,6 @@ export class ArrayWindow extends React.PureComponent {
         super(props);
         this.array = props.array
         this.index = props.index
-        // console.log(this.index)
         this.mainArray = props.mainArray
         this.height = props.height
         this.visualProps = props.visualProps
@@ -48,12 +47,6 @@ export class ArrayWindow extends React.PureComponent {
         this.updateAnimFrame = requestAnimationFrame(this.renderArray.bind(this))
     }
 
-
-    updateState(){
-        // this.setState({
-        //     renderedArray: this.renderArray()
-        // })
-    }
     componentWillReceiveProps(nextProps, nextContext){
         this.array = nextProps.array
         // this.index = nextProps.index
@@ -73,19 +66,18 @@ export class ArrayWindow extends React.PureComponent {
             this.cancelAndUpdate()
             this.updateCounter=1
         }
-        // this.updateState()
-        // this.cancelAndUpdate()
+
     }
 
     renderArray() {
-        // console.log(this.index)
+        const rem = parseInt(getComputedStyle(document.documentElement).fontSize)
+
         let canvasArr = document.getElementsByClassName(styles.canvas)
         let containerArr = document.getElementsByClassName(styles.arrayContainer)
 
         let canvas = canvasArr[canvasArr.length - this.index-1]
         let container = containerArr[containerArr.length - this.index-1]
-        // console.log(canvas)
-        // console.log(container)
+
         if(canvas==null){
             return
         }
@@ -121,76 +113,19 @@ export class ArrayWindow extends React.PureComponent {
 
             }
         }
-        // }else if(this.visualStyle === "dots"){
-        //
-        // }
+        else if(this.visualStyle === "dots"){
+            for (let i = 0; i < this.arrayLen; ++i) {
+                let height = this.array[i].getValue() / (this.mainArray.length - 1) * containerHeight
 
+                let x = offset * i
+                let y = containerHeight - height
+                let w = 0.5*rem
+                let h = 0.5*rem
 
-
-        // let tmp = []
-        // //removed flex-box for optimization
-        // if (this.visualStyle === "bars") {
-        //     let border = this.borderEnabled ? {} : {border: "none"}
-        //     let offset = 2
-        //     if(document.documentElement.containerWidth / this.mainArray.length < 5){
-        //         border = {border: "none"}
-        //         offset = 0
-        //     }
-        //     let width = {
-        //         width: document.documentElement.containerWidth/this.mainArray.length - offset + "px"
-        //     }
-        //     for (let i = 0; i < this.arrayLen; ++i) {
-        //         let styleSheet = {
-        //             height: this.array[i].getValue() / (this.mainArray.length-1) * 100 + "%",
-        //             backgroundColor: "rgb(" + this.array[i].getColorForRender() + ")",
-        //             ...border,
-        //             ...width
-        //         }
-        //         tmp.push(<div key={i} style={styleSheet} className={styles.bar}/>);
-        //     }
-        //     let styleSheet = {
-        //         height: "0%",
-        //         backgroundColor: "rgb(255,255,255)",
-        //         ...border
-        //     }
-        //     for (let i = this.arrayLen; i < this.mainArray.length; ++i) {
-        //         tmp.push(<div key={i} style={styleSheet} className={styles.bar}/>);
-        //     }
-        // } else if (this.visualStyle === "dots") {
-        //     let width = {
-        //         width: document.documentElement.containerWidth/this.mainArray.length + "px"
-        //     }
-        //     for (let i = 0; i < this.arrayLen; ++i) {
-        //         let value = this.array[i].getValue()
-        //         let height = (value === -1) ? {height: 0} : {}
-        //         let styleSheet = {
-        //             bottom: "calc(" + value / this.mainArray.length * 100 + "% - 0.25rem)",
-        //             backgroundColor: "rgb(" + this.array[i].getColorForRender() + ")",
-        //             ...height
-        //         }
-        //         tmp.push(<div style={width} className={styles.dotContainer}>
-        //             <div key={i} style={styleSheet} className={styles.dot}/>
-        //         </div>);
-        //     }
-        //     let styleSheet = {
-        //         bottom: 0,
-        //         backgroundColor: "rgb(0, 0, 0)",
-        //     }
-        //     for (let i = this.arrayLen; i < this.mainArray.length; ++i) {
-        //         tmp.push(<div className={styles.dotContainer}>
-        //             <div key={i} style={styleSheet} className={styles.dot}/>
-        //         </div>);
-        //     }
-        // }
-        // //workaround
-        // let workAroundStyle = {
-        //     width: 0,
-        //     height: 100+"%",
-        //     backgroundColor: "black"
-        // }
-        // tmp.push(<div key={this.mainArray.length+1} style={workAroundStyle} className={styles.bar}/>);
-        //
-        // return tmp
+                ctx.fillStyle = "rgb(" + this.array[i].getColorForRender() + ")"
+                ctx.fillRect(x, y, w, h)
+            }
+        }
     }
 
     render() {
